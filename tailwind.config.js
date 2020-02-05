4a763e05515a050330e19f1248caa6f322518ffc
achourali <achourali1999@gmail.com>
2020-02-05 02:09:18 +0100

    add theme style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
+		colors: {
+			gray: '#69665C',
+			'gray-light': '#75746e',
+			border: '#f6f6f5',
+			milk: '#FFF9DE',
+			white: '#FFFFFF',
+			'white-smoke': '#f9f9f8',
+			lavender: '#D1E5F7',
+			'green-light': '#DAF2D6',
+			pink: '#FFCECE',
+			purple: '#D2CEFF',
+			shadow: '#69665c66'
+		},
 		extend: {}
 	},
 	plugins: []
