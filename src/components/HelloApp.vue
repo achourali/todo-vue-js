9e881a1dd73d4f9daecfa8ed169e855c91d46cfe
achourali <achourali1999@gmail.com>
2020-02-05 02:18:11 +0100

    adding welcome page and images assets and loader component

---
+++ src/components/HelloApp.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="flex flex-col items-center justify-center h-full">
+		<h1 class="text-5xl text-center">
+			<span>t</span>
+			<span>o</span>
+			<span>d</span>
+			<span>o</span>
+		</h1>
+		<p class="text-center py-10">
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam, sed quasi consequatur placeat
+			aut molestias consequuntur veritatis.
+		</p>
+		<router-link to="/add" class="btn mb-12">Get Started</router-link>
+		<div class="flex items-center">
+			<div class="img-wrapper">
+				<img src="@/assets/images/image-1.png" alt="" />
+			</div>
+			<div class="img-wrapper">
+				<img src="@/assets/images/image-2.png" alt="" />
+			</div>
+			<div class="img-wrapper">
+				<img src="@/assets/images/image-3.png" alt="" />
+			</div>
+		</div>
+		<Todo class="w-full" :todo="todo" />
+	</div>
+</template>
+
+<script>
+import Todo from '@/components/Todo.vue';
+
+export default {
+	name: 'HelloApp',
+	components: {
+		Todo
+	},
+	data() {
+		return {
+			todo: {
+				title: 'Download todo app',
+				description: 'he first step for better life',
+				complete: true,
+				tags: ['work', 'study']
+			},
+
+			colors: ['#D2CEFF', '#FFCECE', '#D1E5F7', '#DAF2D6'],
+			color: null
+		};
+	},
+	methods: {
+		randomColor() {
+			document.querySelectorAll('h1 span').forEach((letter) => {
+				this.color = this.colors[Math.floor(Math.random() * this.colors.length)];
+				letter.style.color = this.color;
+			});
+		}
+	},
+	mounted() {
+		this.randomColor();
+	}
+};
+</script>
+
+<style></style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,57 @@
+<template>
+	<div v-if="!isLoading">
+		<div class="h-screen w-screen fixed top-0 z-10 flex justify-center items-center">
+			<h1 class="text-3xl">TODO</h1>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Loader',
+	data() {
+		return {
+			isLoading: false
+		};
+	},
+	mounted() {
+		setTimeout(() => {
+			this.isLoading = true;
+		}, 2500);
+	},
+	unmounted() {
+		this.isLoading = false;
+	}
+};
+</script>
+
+<style scoped>
+div {
+	background: theme('colors.milk');
+	color: theme('colors.gray');
+}
+
+h1 {
+	position: relative;
+}
+h1::before {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	content: '';
+	height: 3px;
+	width: 0%;
+	background: theme('colors.gray');
+	animation: line 1s ease-in forwards 0.5s;
+}
+
+@keyframes line {
+	from {
+		width: 0%;
+	}
+	to {
+		width: 120%;
+	}
+}
+</style>
